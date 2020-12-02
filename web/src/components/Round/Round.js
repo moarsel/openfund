2feec5b5d9a1b5d0b5d3adf890fef28a542cd11a
Russell <russell.hoy@gmail.com>
2020-12-01 19:08:12 -0500

    Update round with end date logic

--- web/src/components/Round/Round.js
+++ web/src/components/Round/Round.js
@@ -1,13 +1,14 @@
-import { Link } from '@redwoodjs/router'
+import { Link, routes } from '@redwoodjs/router'
 import React from 'react'
 import { currency } from 'src/utils'
 import { Card, Lead, Body } from '../UI'
 
 export function Round({ fundingRound }) {
-  return (
+  const isRoundCurrent = new Date(fundingRound.endDate) > new Date()
+  return isRoundCurrent ? (
     <Card className="max-w-xl p-5">
       <Lead>Contribute today!</Lead>
-      <Body className="mt-2 ">
+      <Body withMargins className="mt-2 ">
         We&apos;re fundraising for the projects below until{' '}
         <strong>
           {new Date(fundingRound.endDate).toLocaleTimeString('en-US', {
@@ -16,12 +17,24 @@ export function Round({ fundingRound }) {
           })}
         </strong>
         . We use{' '}
-        <Link href="/about" className="font-bold underline">
+        <Link to={routes.about()} className="font-bold underline">
           Quadratic Funding
         </Link>
         , so your contribution will determine how we distribute an extra{' '}
         <strong>{currency(fundingRound.matchingAmountPool)}</strong> to the
         projects that the community needs.
+      </Body>
+    </Card>
+  ) : (
+    <Card className="max-w-xl p-5">
+      <Lead>Thanks for joining our fundraiser!</Lead>
+      <Body withMargins className="mt-2">
+        This funding round has now concluded. We will use{' '}
+        <Link to={routes.about()} className="font-bold underline">
+          Quadratic Funding
+        </Link>
+        , so that all contributions we received will determine how we distribute
+        our additional funds to the projects that the community needs.
       </Body>
     </Card>
   )
--- web/src/components/UI/Typography/Body.js
+++ web/src/components/UI/Typography/Body.js
@@ -6,7 +6,7 @@ export const Body = ({
   ...props
 }) => {
   const Component = as
-  const marginClasses = withMargins ? 'mb-2 mt-4 max-w-lg' : ''
+  const marginClasses = withMargins ? 'mb-2 mt-3 max-w-xl' : ''
   return (
     <Component
       className={`text-md font-body text-gray-900 sm:text-lg ${marginClasses} ${className}`}
