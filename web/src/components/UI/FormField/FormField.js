ea983b1d40ee8d2315d68c12974a00a6c51a4b2d
Russell <russell.hoy@gmail.com>
2020-12-01 20:19:55 -0500

    better fieldset support in formfield

--- web/src/components/UI/FormField/FormField.js
+++ web/src/components/UI/FormField/FormField.js
@@ -10,22 +10,22 @@ export function FormField({
   ...props
 }) {
   const Component = as
+  const LabelComponent = as === 'fieldset' ? 'legend' : 'span'
   const fieldId = `form-field-${useId()}`
   const describedById = `description-${fieldId}`
   return (
     <>
-      <Component
-        htmlFor={fieldId}
-        className={`block label mb-1 text-gray-700 ${className}`}
-        {...props}
-      >
-        {label}
-      </Component>
-      {children &&
-        React.cloneElement(children, {
-          id: fieldId,
-          'aria-describedby': describedById,
+      <Component className={className} {...props}>
+        <LabelComponent className={`block label mb-1 text-gray-700 `}>
+          {label}
+        </LabelComponent>
+        {React.Children.map(children, (child, i) => {
+          return React.cloneElement(child, {
+            id: `${fieldId}-${i}`,
+            'aria-describedby': describedById,
+          })
         })}
+      </Component>
       <span
         className="block"
         style={description ? { minHeight: '2rem' } : {}}
