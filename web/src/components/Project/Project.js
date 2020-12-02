0ab525902304d784fa13cde30ea6e8a069a9b667
Russell <russell.hoy@gmail.com>
2020-12-01 20:20:38 -0500

    implement project page round close

--- web/src/components/Project/Project.js
+++ web/src/components/Project/Project.js
@@ -9,6 +9,7 @@ import { ContributionBar } from '../UI/ContributionBar/ContributionBar'
 import { SocialShare } from '../UI/SocialShare/SocialShare'
 
 const Project = ({ project, endDate }) => {
+  const isRoundCurrent = new Date(endDate) > new Date()
   const { addItem } = useCart()
   const [amount, setAmount] = useState(2000)
 
@@ -31,7 +32,7 @@ const Project = ({ project, endDate }) => {
   }
 
   return (
-    <div className="relative grid grid-cols-1 gap-4 md:grid-cols-3 justify-items-auto">
+    <div className="relative grid grid-cols-1 gap-4 lg:grid-cols-3 justify-items-auto">
       <div className="col-span-2">
         <img
           src={project.coverImage}
@@ -70,77 +71,86 @@ const Project = ({ project, endDate }) => {
           goalAmount={project.goalAmount}
         />
         <div className="p-6">
-          <Body className="mb-1">
+          <Body as="div" className="mb-1">
             <Lead as="h3" className="mb-1">
-              Your Contribution
+              {isRoundCurrent ? 'Your Contribution' : 'Contributions'}
             </Lead>
             <strong>{project.contributorCount}</strong> people have raised{' '}
-            <strong>{currency(project.contributionsTotal)}</strong> so far.
-            Right now, that unlocks{' '}
-            <strong>{currency(project.currentMatchingAmount)}</strong> of
-            matching funds.
+            <strong>{currency(project.contributionsTotal)}</strong>. That
+            unlocks <strong>{currency(project.currentMatchingAmount)}</strong>{' '}
+            of matching funds.
           </Body>
-          <Body>
-            Will you help them reach their{' '}
-            <strong>{currency(project.goalAmount)}</strong> goal by{' '}
-            <strong>
-              {new Date(endDate).toLocaleDateString('en-US', {
-                month: 'long',
-                day: 'numeric',
-              })}
-            </strong>
-            ?
-          </Body>
+          {isRoundCurrent && (
+            <Body>
+              Will you help them reach their{' '}
+              <strong>{currency(project.goalAmount)}</strong> goal by{' '}
+              <strong>
+                {new Date(endDate).toLocaleDateString('en-US', {
+                  month: 'long',
+                  day: 'numeric',
+                })}
+              </strong>
+              ?
+            </Body>
+          )}
         </div>
-        <form onSubmit={() => handleSubmit(amount)} className="text-center">
-          <div className="flex flex-wrap items-end justify-start px-6 pb-6 text-left">
-            <div className="w-40 mr-2 ">
-              <FormField label="Amount (USD)">
+        {isRoundCurrent && (
+          <form onSubmit={() => handleSubmit(amount)} className="m-6">
+            <FormField
+              as="fieldset"
+              label="Amount (USD)"
+              className="flex flex-wrap items-end justify-start"
+            >
+              <div className="w-24 mr-2">
                 <TextField
                   type="number"
                   name="amount"
                   value={amount / 100}
                   onChange={(e) => setAmount(e.target.value * 100)}
-                  className=" input-field"
+                  className="input-field"
                   required
                   min="5"
                 />
-              </FormField>
-            </div>
-            <div className="mt-2">
-              <Button
-                className={`w-12 mr-2 ${amount === 2000 ? ' bg-gray-300' : ''}`}
-                variant="secondary"
-                aria-pressed={amount === 2000}
-                onClick={() => setAmount(2000)}
-              >
-                $20
-              </Button>
-              <Button
-                className={`w-12 mr-2 ${amount === 5000 ? ' bg-gray-300' : ''}`}
-                variant="secondary"
-                aria-pressed={amount === 5000}
-                onClick={() => setAmount(5000)}
-              >
-                $50
-              </Button>
-              <Button
-                className={`w-12 mr-2 ${
-                  amount === 10000 ? ' bg-gray-300' : ''
-                }`}
-                aria-pressed={amount === 10000}
-                variant="secondary"
-                onClick={() => setAmount(10000)}
-              >
-                $100
-              </Button>
-            </div>
-          </div>
+              </div>
+              <div className="mt-2">
+                <Button
+                  className={`w-12 mr-2 ${
+                    amount === 2000 ? ' bg-gray-300' : ''
+                  }`}
+                  variant="secondary"
+                  aria-pressed={amount === 2000}
+                  onClick={() => setAmount(2000)}
+                >
+                  $20
+                </Button>
+                <Button
+                  className={`w-12 mr-2 ${
+                    amount === 5000 ? ' bg-gray-300' : ''
+                  }`}
+                  variant="secondary"
+                  aria-pressed={amount === 5000}
+                  onClick={() => setAmount(5000)}
+                >
+                  $50
+                </Button>
+                <Button
+                  className={`w-12 mr-2 ${
+                    amount === 10000 ? ' bg-gray-300' : ''
+                  }`}
+                  aria-pressed={amount === 10000}
+                  variant="secondary"
+                  onClick={() => setAmount(10000)}
+                >
+                  $100
+                </Button>
+              </div>
+            </FormField>
 
-          <Button type="submit" className="mb-4">
-            Contribute {currency(amount)}
-          </Button>
-        </form>
+            <Button type="submit" className="my-4">
+              Contribute {currency(amount)}
+            </Button>
+          </form>
+        )}
       </Card>
     </div>
   )
